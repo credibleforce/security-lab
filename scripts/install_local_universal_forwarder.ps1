2a255fce4f65b207c0e2d80b1aeb2d671860186d
scalatar <jamiefrasermcmurray@gmail.com>
2021-06-22 17:00:48 -0700

    Bump uf 8.2.0

--- scripts/install_local_universal_forwarder.ps1
+++ scripts/install_local_universal_forwarder.ps1
@@ -8,11 +8,11 @@ param(
 
 If (-not (Test-Path "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe")) {
     Write-Host "Downloading Splunk Universal Forwarder"
-    $msiFile = $env:Temp + "\splunkforwarder-8.0.2-a7f645ddaf91-x64-release.msi"
+    $msiFile = $env:Temp + "\splunkforwarder-8.2.0-e053ef3c985f-x64-release.msi"
 
     Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing & Starting Splunk"
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-    (New-Object System.Net.WebClient).DownloadFile('https://www.splunk.com/page/download_track?file=8.0.2/windows/splunkforwarder-8.0.2-a7f645ddaf91-x64-release.msi&ac=&wget=true&name=wget&platform=Windows&architecture=x86_64&version=8.0.2&product=universalforwarder&typed=release', $msiFile)
+    (New-Object System.Net.WebClient).DownloadFile('https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=8.2.0&product=universalforwarder&filename=splunkforwarder-8.2.0-e053ef3c985f-x64-release.msi&wget=true', $msiFile)
     Start-Process -FilePath "c:\windows\system32\msiexec.exe" -ArgumentList '/i', "$msiFile", $('DEPLOYMENT_SERVER="{0}:8089"  AGREETOLICENSE=Yes SERVICESTARTTYPE=auto LAUNCHSPLUNK=1 SPLUNKPASSWORD="{1}" /quiet' -f ${SplunkDeploymentServer},${SplunkPassword}) -Wait
 } Else {
     Write-Host "Splunk is already installed. Moving on."
